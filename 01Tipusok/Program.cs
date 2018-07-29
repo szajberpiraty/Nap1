53e686bc11fdd684b331666928bd866d43366b4b
szajberpiraty <szajberpiraty@gmail.com>
2018-07-29 18:02:58 +0200

    Kezdés

--- 01Tipusok/Program.cs
+++ 01Tipusok/Program.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace _01Tipusok
 {
+    //A fordítás célja egy MSIL (köztes nyelv) nyelvű állomány létrehozása. A köztes nyelvet a Dotnet keretrendszer futtatja (Linuxon Mono)
+    //Igény arra, hogy a program több eszközön is fusson.
+    //Programkönyvtárak : egységesebb felület a programozók felé
+    //C++ oop lehetőségek, de számos dolog a programozó kezében marad, memóriakezelés, stb.
+    //Megjelent a JAVA - köztes kód
+    //.NET ->futtató keretrendszer. C# ->.NET hez való C gyakorlatilag, kényelmesen használható futtatási és programozási környezet
+    //MSIL - intermediate language
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
