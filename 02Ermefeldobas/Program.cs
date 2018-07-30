7c4beea1caa39742f75a445b833578bc729d0792
szajberpiraty <szajberpiraty@gmail.com>
2018-07-30 15:12:38 +0200

    OOP alapok

--- 02Ermefeldobas/Program.cs
+++ 02Ermefeldobas/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace _02Ermefeldobas
 {
+    //OOP alapelvek
+    //Elvonatkoztatás (absztrakció)-> a számunkra fontos dolgok kiemelése
+    //Egységbe zárás (encapsulation)-> csak azokat mutatja az objektum, ami a külvilágra tartozik. Példa: távirányító
+    //Modularitás (a problémát kisebb problémákra osztjuk, azokat oldjuk meg)
+    //Hierarchia a modulokat egymás alá-fölé lehet rendelni
+
+    //OOP programozás
+    //Objektum 
+    //  1. azonosíthatóság (identity)
+    //  2. van állapota (state)
+    //  3. van viselkedése (behaviour)
+    //
+    //Osztály
+    // Absztrakció, az objektum terve, ezt példányosítjuk
+
+
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
