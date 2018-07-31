0d2039dfca4baf0ed5c10d918449c198b53f32f0
szajberpiraty <szajberpiraty@gmail.com>
2018-07-31 20:51:43 +0200

    Metódusok, overload, szignatúra, viselkedés

--- 03OsztalyokHaziallatok/Program.cs
+++ 03OsztalyokHaziallatok/Program.cs
@@ -9,7 +9,12 @@ namespace _03OsztalyokHaziallatok
     //Objektum: 
     //-egyértelmű határok
     //-állapota van
-    //-működése van
+    //-viselkedése van
+    //Folytatás 29.17-től
+    //függvény szignatúra: a függvény neve és paramétereinek listája
+    //overload: ugyanazzal a névvel több függvényt is lehet csinálni, ha különbözik a szignatúrájuk
+    
+    
     class Program
     {
         static void Main(string[] args)
@@ -34,6 +39,17 @@ namespace _03OsztalyokHaziallatok
             haziallat2.HanyFogaVan = 34;
             var szemek = haziallat2.HanySzeme;
 
+            //érték szerinti átadás
+            var lepes = 5;
+            haziallat.LepjenEnnyit(lepes);
+
+            //referencia átadása
+            haziallat.LepjenEnnyitR(ref lepes);
+            Console.WriteLine(lepes);
+
+            haziallat.AllatNeve();
+
+
             Console.ReadKey();
         }
         class Haziallat
@@ -56,7 +72,53 @@ namespace _03OsztalyokHaziallatok
             public int HanySzeme { get; set; }
             private int hanyFogaVan;
             //csak settert automatikusan nem lehet beállítani
+            //a get és a set lehet private is
             public int HanyFogaVan {  set { hanyFogaVan = value; } }
+
+            //ha mi akarjuk implementálni a gettert
+            private int hanyFarkaVan;
+            public int HanyFarkaVan
+            {
+
+                get
+                {
+                    return hanyFarkaVan;
+                }
+
+                set
+                {
+                    hanyFarkaVan = value;
+                }
+
+
+                
+            }
+
+            //Viselkedés definíció
+            public void LepjenEnnyit(int lepes)
+            {
+                Console.WriteLine("Ennyit lépek:{0}",lepes);
+            }
+
+            public void LepjenEnnyitR(ref int lepes)
+            {
+                Console.WriteLine("Ennyit lépek:{0}", lepes);
+                lepes = 4;
+            }
+            //Kimenő paraméter megadása, bejövő adatot ez nem fogad
+            public void LepjenEnnyitOut(ref int lepes,out int Kimeno)
+            {
+                Console.WriteLine("Ennyit lépek:{0}", lepes);
+                lepes = 4;
+                Kimeno = 1;
+            }
+
+            //Beállító metódus alapértelmezett értékkel
+            String allatNev;
+            public void AllatNeve(String nev="Bubu")
+            {
+                allatNev = nev;
+            }
 
         }
     }
