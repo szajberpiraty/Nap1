c72582260b6e3926d44f56cc03a815f26618d6fd
szajberpiraty <szajberpiraty@gmail.com>
2018-08-01 16:15:06 +0200

    Konstruktor év véglegesítő kezdés

---
+++ FeluletSikidomok/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeluletSikidomok
+{
+    //Névterek
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            Console.WriteLine("Zu");
+            sw.Stop();
+            Console.WriteLine("Eltelt:{0}",sw.ElapsedTicks);
+            var alap = new Alap();
+            var leszarmazott = new Leszarmaztatott();
+
+
+
+
+
+            Console.ReadKey();
+        }
+
+        class Alap
+        {
+            //ha nincs konstruktor, akkor a fordító csinál alapértelmezett paraméter nélküli konstruktort
+            public Alap()
+            {
+
+            }
+        }
+
+        class Leszarmaztatott : Alap
+        {
+
+        }
+    }
+}
