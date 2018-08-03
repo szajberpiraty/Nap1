fb478bd83adc3f21cac287c017406b23c58531fb
szajberpiraty <szajberpiraty@gmail.com>
2018-08-03 10:59:31 +0200

    Memória felzabáltatás, GC futásának vizsgálata, using minta

---
+++ 08GarbageCollector2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08GarbageCollector2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (!Console.KeyAvailable)
+            {
+                //var stream = new MemoryStream(100000); //Folyamatosan megy a szemétgyűjtő
+                var bitmap = new Bitmap(1280,1024);//Van véglegesítője, ezért nagyon könnyen elfogy a memória
+                //using( var bitmap = new Bitmap(1280,1024)) {} -Így nem lesz memória probléma
+            }
+        }
+    }
+}
