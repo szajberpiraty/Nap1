d0ef7d134ac03ee27ed8d40ab3a2d394bea4ddf1
szajberpiraty <szajberpiraty@gmail.com>
2018-08-03 11:57:02 +0200

    Memóriafoglalások

--- 09IDisposable/Program.cs
+++ 09IDisposable/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,19 +17,19 @@ namespace _09IDisposable
 
             //Ez kódblokk a try ágban megpróbálja végrahajtani a parancsokat(vagy sikerül, vagy nem), utána a finally ágban lévő kódot mindenképp végrehajtja
 
-            var tisztalev2 = new TisztaLevego();
-            //A using-ot a fordító egy ilyen kódblokká alakítja
-            try
-            {
+            //var tisztalev2 = new TisztaLevego();
+            ////A using-ot a fordító egy ilyen kódblokká alakítja
+            //try
+            //{
                 
-            }
-            finally//akármi van, lefut
-            {
-                if (tisztalev2!=null)
-                {
-                    tisztalev2.Dispose();
-                }
-            }
+            //}
+            //finally//akármi van, lefut
+            //{
+            //    if (tisztalev2!=null)
+            //    {
+            //        tisztalev2.Dispose();
+            //    }
+            //}
 
         }
 
@@ -36,11 +37,22 @@ namespace _09IDisposable
     class TisztaLevego : IDisposable
     {
         //File stream esetében "kötelesek" (gentleman agreement) vagyunk megvalósítani az IDisposable-t.
+        Stream menedzseltStream = new FileStream("testfile.txt", FileMode.Create);
+
         //Ha nem menedzselt memóriát használunk
-        IntPtr nemmenedzseltmem = IntPtr.Zero; //Itt kikerül a szemétgyűjtő fennhatósága alól a memória terület, a megfelelő felszabadításról nekünk kell gondoskodni
+        IntPtr NemMenedzseltMemoria = IntPtr.Zero; //Itt kikerül a szemétgyűjtő fennhatósága alól a memória terület, a megfelelő felszabadításról nekünk kell gondoskodni
 
-
-        Stream ms = new FileStream("testfile.txt",FileMode.Create);
+        //HA nagy méretű menedzselt memóriát használunk
+        List<string> menedzseltLista= new List<string>();
+        public TisztaLevego()
+        {
+            NemMenedzseltMemoria = Marshal.AllocHGlobal(1000000);//Lefoglalok 1000000 byte-ot
+            GC.AddMemoryPressure(1000000);//A szemétgyűjtő nem kezeli azt amit én lefoglaltam, közölni kell vele, hogy mennyit foglaltam le
+            for (int i = 0; i < 1000000; i++)
+            {
+                menedzseltLista.Add(new string('A',1));
+            }
+        }
 
         ~TisztaLevego()
         {
@@ -57,7 +69,10 @@ namespace _09IDisposable
 
         private void Dispose(bool dispose)
         {
-            throw new NotImplementedException();
+            if (dispose)
+            {
+                //még nem futott le a szemétgyűjtés
+            }
         }
     }
 
