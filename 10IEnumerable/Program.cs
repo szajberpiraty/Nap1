c4d29c79f712473f343f6ec27724163ad63d7beb
szajberpiraty <szajberpiraty@gmail.com>
2018-08-04 12:18:00 +0200

    Bejárható osztály implementáció

--- 10IEnumerable/Program.cs
+++ 10IEnumerable/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,10 +11,10 @@ namespace _10IEnumerable
     {
         static void Main(string[] args)
         {
-            foreach (var listaElem in BevasarloLista())
-            {
-                Console.WriteLine(listaElem);
-            }
+            //foreach (var listaElem in BevasarloLista())
+            //{
+            //    Console.WriteLine(listaElem);
+            //}
 
             //Console.WriteLine("1 kg kenyér");
             //Console.WriteLine("10 dkg szalámi");
@@ -37,6 +38,53 @@ namespace _10IEnumerable
             yield return "lakat";
             yield return "póni";
             
+        }
+        class BejarhatoOsztaly : IEnumerable
+        {
+            List<string> lista = new List<string>();
+            public void Add(string s)
+            {
+                lista.Add(s);
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                return new Bejaro(lista);
+            }
+        }
+        class Bejaro : IEnumerator
+        {
+            private List<string> lista;
+            int pozicio = -1;
+
+
+            public Bejaro(List<string> lista)
+            {
+                this.lista = lista;
+            }
+
+            public object Current
+            {
+                get
+                {
+                    var current = lista[pozicio];
+                    Console.WriteLine("Aktuális pozíció:{0}",current);
+                    return current;
+                }
+            }
+
+            public bool MoveNext()
+            {
+                pozicio++;
+                var vanMegElem = pozicio < lista.Count();
+                Console.WriteLine("Pozicio:{0},Van még:{1}",pozicio,vanMegElem);
+                return vanMegElem;
+            }
+
+            public void Reset()
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
