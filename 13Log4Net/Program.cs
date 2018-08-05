86849ce7e2236a87d8463166a31173f803c68f86
szajberpiraty <szajberpiraty@gmail.com>
2018-08-05 16:30:13 +0200

    Log4net első működő

--- 13Log4Net/Program.cs
+++ 13Log4Net/Program.cs
@@ -9,12 +9,17 @@ namespace _13Log4Net
 {
     class Program
     {
+        private static readonly log4net.ILog log= log4net.LogManager.GetLogger("_13Log4Net program");
         static void Main(string[] args)
         {
             //Nagyon egyszerű nyomkövetés
             //PeldaNaplo();
-
-
+            log4net.Config.XmlConfigurator.Configure();//appconfig konfigot betölti, létrehozza a környezetet
+            for (int i = 0; i < 10; i++)
+            {
+                log.Debug("Naplóüzenet a log4net-ből");
+            }
+            Console.ReadLine();
 
         }
 
