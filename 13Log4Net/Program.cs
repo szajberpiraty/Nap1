be01ac4eab5b2eab1e22a06c73b5a341ce9a5543
szajberpiraty <szajberpiraty@gmail.com>
2018-08-05 14:42:57 +0200

    Végtelenül egyszerű nyomkövetés

---
+++ 13Log4Net/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13Log4Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nagyon egyszerű nyomkövetés
+            //PeldaNaplo();
+
+
+
+        }
+
+        private static void PeldaNaplo()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Debug.WriteLine("Hibakeresés:{0}", i);
+            }
+        }
+    }
+}
