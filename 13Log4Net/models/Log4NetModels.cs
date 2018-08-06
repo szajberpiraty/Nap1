972c6871945c1b0064b7f72634fc9dc1c58afac0
szajberpiraty <szajberpiraty@gmail.com>
2018-08-06 11:25:42 +0200

    Entity Framework telepítés, dbcontext

---
+++ 13Log4Net/models/Log4NetModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13Log4Net.models
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Thread { get; set; }
+        public string Level { get; set; }
+        public string Logger { get; set; }
+        public string Message { get; set; }
+        public string Exception { get; set; }
+
+
+
+    }
+}
---
+++ 13Log4Net/models/Log4netContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13Log4Net.models
+{
+    public class Log4netContext:DbContext
+    {
+        public DbSet<Log> dbSet { get; set; }
+    }
+}
