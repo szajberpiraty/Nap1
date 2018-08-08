a1647666c3b666709c6c3b5670188dfd6665381b
szajberpiraty <szajberpiraty@gmail.com>
2018-08-08 16:36:35 +0200

    Lambdás függvényhívás

--- 16StrategiaDelegateTel/Program.cs
+++ 16StrategiaDelegateTel/Program.cs
@@ -13,7 +13,27 @@ namespace _16StrategiaDelegateTel
         static void Main(string[] args)
         {
             var adatOsztaly = new AdatOsztaly(adatok: new List<int>(new int[] { 1, 2, 3, 4, 5, 6, 11, 22 }));
+
+            //1.delegate definíció
+            //2.függvény definíció
+            //3.fogadni a delegate tipust
+            //4.hívás
+
+
             var eredmeny = adatOsztaly.Muvelet(ListaOsszegzes); //Itt már három lépésből meg lehet adni a műveletet az osztálynak
+
+            //Tömörebben
+            //1.Kell egy függvény definíció
+            //2.Fogadni a delegate típust
+            //3.hívás func
+
+
+            var eredmeny2 = adatOsztaly.Muvelet2(ListaOsszegzes);
+
+            //Egy soros lambdás megoldás
+            //1.Fogadni a delegate típust a Func segítségével
+            //2. Lambda segítségével beküldjük a stratégiánkat
+            var eredmeny3 = adatOsztaly.Muvelet3(x=>x.Sum());
 
         }
         static int ListaOsszegzes(List<int> adatok)
@@ -39,6 +59,16 @@ namespace _16StrategiaDelegateTel
             }
             return muveletLista(adatok);
 
+        }
+
+        internal object Muvelet2(Func<List<int>, int> listaOsszegzes)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Muvelet3(Func<List<int>, int> listaOsszegzes)
+        {
+            throw new NotImplementedException();
         }
     }
 }
