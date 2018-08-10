813875a98216ce9ac402c87e1b0cb62c10008412
szajberpiraty <szajberpiraty@gmail.com>
2018-08-10 12:50:20 +0200

    Bankszámla jó megoldás eventekkel

---
+++ 18EsemenyekMegoldas/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18EsemenyekMegoldas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bankszamla = new Bankszamla();
+            bankszamla.ErtesitesiHivasLista += delegate { Console.WriteLine("Minuszba menté'!!"); };
+            bankszamla.Jovairas(osszeg: 500);
+
+            //Probléma, hogy ezt meg lehet csinálni, így elvesztem az egységbezárást, ez ezt teszi
+            //bankszamla.ErtesitesiHivasLista = null;
+
+            //Probléma, hogy kívülről meg lehet hívni a híváslistát
+            //bankszamla.ErtesitesiHivasLista();
+
+            bankszamla.Jovairas(osszeg: -5500);
+
+
+
+            Console.ReadLine();
+            //Nem engedhetem, hogy az egyenleget kívülről lehessen írni, ezért a property setterét private-ra kell állítani
+        }
+    }
+    class Bankszamla
+    {
+        //Ezt lehet, de nem feltétlenül szükséges
+        //public Bankszamla()
+        //{
+        //    Egyenleg = 0;
+        //}
+        //delegate-el próbáljuk megoldani, hogy a számla értesítsen minket
+        //public delegate void MinuszEgyenlegErtesitesiFvDef();
+
+        //public MinuszEgyenlegErtesitesiFvDef ErtesitesiHivasLista=null;
+
+        //Az action egyszerűbb megoldás
+        //public Action ErtesitesiHivasLista = null;
+
+        //Ezek helyett Event-et használunk
+        //Event az  egy speciális delegate
+        //Csak void metódust használhatunk, nincs visszatérési érték
+        //Nem lehet az osztályon kívül hívni
+        //Nem lehet az osztályon kyvül (=-vel) értéket adni,így felülírni a híváslistát
+        // A delegate funkció kiváltására az EventHandler szolgál
+
+        public event EventHandler ErtesitesiHivasLista = null;
+
+        public int Egyenleg { get; private set; }
+
+        public void Jovairas(int osszeg)
+        {
+            Egyenleg += osszeg;
+            Console.WriteLine("Összeg:{0},új egyenleg{1}", osszeg, Egyenleg);
+            if (Egyenleg < 0)
+            {
+                //Értesíteni kell a tulajdonost
+                var hivaslista = ErtesitesiHivasLista;
+                if (hivaslista != null)
+                {
+                    hivaslista();
+                }
+            }
+        }
+    }
+}
