0b10decc240e99ece3c8db27d68b82441dd0a8f0
szajberpiraty <szajberpiraty@gmail.com>
2018-08-12 13:50:59 +0200

    Stream-ek kezdés

---
+++ 19StreamekStreamReader/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19StreamekStreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = "teszt.txt";
+            File.WriteAllText(filename,
+                string.Format("Ez a kiírandó tartalom{0},{1},{2},{3}"
+                , Environment.NewLine
+                , (char)113
+                , Convert.ToChar(119)
+                , '\u0027')
+                , Encoding.UTF8);
+        }
+    }
+}
