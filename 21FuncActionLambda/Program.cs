a9f60d97cf874aa5bee6e18bd24842ed2833e36f
szajberpiraty <szajberpiraty@gmail.com>
2018-08-13 12:23:55 +0200

    Negyzetre emelés lambda funkcióval

--- 21FuncActionLambda/Program.cs
+++ 21FuncActionLambda/Program.cs
@@ -20,7 +20,26 @@ namespace _21FuncActionLambda
 
             NegyztreEmelesDef hivaslista = NegyzetreEmeles;
             Console.WriteLine(NegyzetreEmeles(2));
+            
+
+            //Ezek helyett kell egy egyszerűbb megoldás
+            //A függvény definíciók kiváltására szolgálnak a lambda kifejezések
+
+            //Első verzió
+            //Bal oldalt a paraméterlista
+            //jobb oldalt a kódblokk
+
+            hivaslista = (x) => { return x * x; };
+
+            //Ha egy kifejezéssel kell visszatérni, akkor nem kell a kódblokk, csak a kifejezés
+            //ha egy paraméter van, nem kell zárójel
+
+            hivaslista += z => z * z;
+            Console.WriteLine(hivaslista(2));
             Console.ReadLine();
+            //Az Action<> és a Func<> előre legyártott, a keretrendszer részét képező delegate definíciók
+             
+
         }
         static int NegyzetreEmeles(int x)
         {
