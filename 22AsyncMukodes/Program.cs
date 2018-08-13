e7e2d699f6982f3ad4aee254991a57c63933c8ff
szajberpiraty <szajberpiraty@gmail.com>
2018-08-13 19:45:08 +0200

    Aszinkron hívások: 2, 3 változat

--- 22AsyncMukodes/Program.cs
+++ 22AsyncMukodes/Program.cs
@@ -41,6 +41,41 @@ namespace _22AsyncMukodes
             Console.WriteLine("+Fő szál aszinkron 1 végzett eredmény {0}", eredmeny01);
 
             //Folytatás 20:54-től
+            //2. lehetőség: pollozás, pollozhatjuk a hívás végét jelző flag-et
+
+            var ar02 = am.BeginInvoke(5, "Aszinkron példa 2", null, null);
+            Console.WriteLine("+Fő szál aszinkron hívás elindult");
+
+            var wait = 0;
+            while (!ar02.IsCompleted)
+            {
+                //Itt lehet egyéb munkát elvégezni
+                Thread.Sleep(300);
+                wait += 300;
+                Console.WriteLine("+Fő szál: várakozunk a második példára {0}",wait);
+            }
+
+            var eredmeny02 = am.EndInvoke(ar02);
+            Console.WriteLine("+Fő szál aszinkron 2 végzett eredmény {0}", eredmeny02);
+
+            //3.Várunk egy ideig az aszinkron végrehajtásra
+            
+
+            var ar03 = am.BeginInvoke(5, "Aszinkron példa 3", null, null);
+            Console.WriteLine("+Fő szál aszinkron hívás elindult");
+
+            wait = 0;
+            //Minden ciklusban várunk, amíg véget ér a feladat, de legfeljebb 300ms-ot
+            while (!ar03.AsyncWaitHandle.WaitOne(300))
+            {
+                //Itt lehet egyéb munkát elvégezni
+                //Thread.Sleep(300);
+                wait += 300;
+                Console.WriteLine("+Fő szál: várakozunk a harmadik példára {0}", wait);
+            }
+
+            var eredmeny03 = am.EndInvoke(ar03);
+            Console.WriteLine("+Fő szál aszinkron 3 végzett eredmény {0}", eredmeny03);
 
             Console.ReadLine();
         }
