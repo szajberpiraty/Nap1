abe3e377373e4b4b6dd89f8a64fd070d99984d40
szajberpiraty <szajberpiraty@gmail.com>
2018-08-13 11:17:11 +0200

    attrib ignorálás

--- 20SerializeDeserialize/Program.cs
+++ 20SerializeDeserialize/Program.cs
@@ -13,12 +13,16 @@ namespace _20SerializeDeserialize
     {
         static void Main(string[] args)
         {
+            var listaadat = new ListaAdat();
+            
+
             var adat = new Adatosztaly() {
                 Egesz = 10,
                 TizedesTort=12.67m,
                 Datum=DateTime.MaxValue,
                 DatumMin=DateTime.MinValue,
                 Szoveg="ÁrvíztűrőTükörfúrógép",
+                Jelszo="Titok",
                 Aladatosztaly=new AlAdatosztaly()
                 {
                     Egesz = 112,
@@ -29,13 +33,22 @@ namespace _20SerializeDeserialize
                 }
             
             };
+
+            listaadat.ListaAdatok.Add(adat);
+            listaadat.ListaAdatok.Add(adat);
+            listaadat.ListaAdatok.Add(adat);
+            listaadat.ListaAdatok.Add(adat);
+
             var filename = "teszt.txt";
-            var serializer = new XmlSerializer(typeof(Adatosztaly));
+            //var serializer = new XmlSerializer(typeof(Adatosztaly));
+            var serializer = new XmlSerializer(typeof(ListaAdat));
             using (var fs = new FileStream(filename, FileMode.Create))
             {
 
                
-                serializer.Serialize(fs,adat);
+                //serializer.Serialize(fs,adat);
+                //A property-jét kell kiírni, különben kivételt dob
+                serializer.Serialize(fs, listaadat);
 
             }
             using (var fs = new FileStream(filename, FileMode.Open))
@@ -54,9 +67,13 @@ namespace _20SerializeDeserialize
     {
         public int Egesz { get; set; }
         public decimal TizedesTort { get; set; }
+        [XmlElement("MasValami")] //Az XML állományban így szerepel
         public DateTime Datum { get; set; }
         public DateTime DatumMin { get; set; }
         public string Szoveg { get; set; }
+        //Meg lehet akadályozni a property fájlba írását
+        [XmlIgnore]
+        public string Jelszo { get; set; }
         public AlAdatosztaly Aladatosztaly { get; set; }
     }
     public class AlAdatosztaly
@@ -69,6 +86,11 @@ namespace _20SerializeDeserialize
     }
     public class ListaAdat
     {
+        public ListaAdat()
+        {
+            //Kell a konstruktor, különben mindig ellenőrizni kell, hogy létezik-e (minden hozzáadás előtt)
+            ListaAdatok = new List<Adatosztaly>(); 
+        }
         public List<Adatosztaly> ListaAdatok { get; set; }
     }
 }
