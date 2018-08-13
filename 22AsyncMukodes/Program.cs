4760964fa6e4878f4e7dfe94f8d516b47b7ee71d
szajberpiraty <szajberpiraty@gmail.com>
2018-08-13 13:55:16 +0200

    Első aszinkron példa

--- 22AsyncMukodes/Program.cs
+++ 22AsyncMukodes/Program.cs
@@ -14,18 +14,23 @@ namespace _22AsyncMukodes
             //függvény létrehozás, int és string paramétert vár, DateTime-ot ad vissza
             Func<int, string, DateTime> am = (ciklusok, nev) => 
             {
-                Console.WriteLine("{0} elindult ciklusok {1}",nev,ciklusok);
+                Console.WriteLine("+-{0} elindult ciklusok {1}",nev,ciklusok);
                 for (int i = 0; i < ciklusok; i++)
                 {
-                    Console.WriteLine("{0} ciklus {1}",nev,i);
+                    Console.WriteLine("+-{0} ciklus {1}",nev,i);
                     Thread.Sleep(400);
                 }
-                Console.WriteLine("{0} végzett", nev);
+                Console.WriteLine("+-{0} végzett", nev);
                 return DateTime.Now;
             };
             var eredmeny0=am(3, "szinkron hívás");
-            Console.WriteLine("Fő szál szinkron hívás elindult");
-            Console.WriteLine("Fő szál végzett eredmény {0}",eredmeny0);
+            Console.WriteLine("+Fő szál szinkron hívás elindult");
+            Console.WriteLine("+Fő szál végzett eredmény {0}",eredmeny0);
+
+            //Szinkron módon így is meg lehetett volna hívni
+            //var eredmeny0=am.Invoke(3,"Szinkron hívás");
+
+            am.BeginInvoke(5, "Aszinkron példa 1", null, null);
 
             Console.ReadLine();
         }
