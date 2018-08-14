053a59e2e5ffe0f82d1bb337a5607206aba2a63a
szajberpiraty <szajberpiraty@gmail.com>
2018-08-14 12:19:42 +0200

    usning nélküli fájl olvasás, némileg egyszerűbb, mert nem kell időzítés, nem kell szemafor

---
+++ 24AsyncFileUsingNelkul/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _24AsyncFileUsingNelkul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filenev = "teszt.txt";
+            using (var fscreate = new FileStream(filenev, FileMode.Create))
+            {
+                fscreate.SetLength(1000000);
+            }
+
+
+
+            //Rossz megoldás lambdával
+            var fs = new FileStream(filenev, FileMode.Open);
+            
+                int pufferMeret = 90000;
+                byte[] puffer = new byte[pufferMeret];
+                AsyncCallback rekurzivCallback = null;
+                AsyncCallback callback = ar =>
+                {
+                    var olvasottByteok = fs.EndRead(ar);
+                    if (olvasottByteok > 0)
+                    {//még nem végeztünk
+                        Console.WriteLine("Beolvasva {0},pozício {1}", olvasottByteok, fs.Position);
+                        fs.BeginRead(puffer, 0, pufferMeret, rekurzivCallback, null);
+                    }
+                    else
+                    {//végeztünk
+                        //beállítom a szemafort
+                      
+                        Console.WriteLine("Végeztünk");
+                        fs.Dispose();
+                    }
+
+                };
+                rekurzivCallback = callback;
+                fs.BeginRead(puffer, 0, pufferMeret, callback, null);
+             
+            
+
+            //Ez a megoldás elszáll, megy az aszinkron hívás, közben a főszál véget ér, így aztán kezeletlen dispose-al meghal a program
+            Console.ReadLine();
+        }
+    }
+}
