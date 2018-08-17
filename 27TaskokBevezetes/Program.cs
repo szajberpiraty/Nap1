1f4cc52881b1f655ac6cbbd1b9b68bb89ce5553d
szajberpiraty <szajberpiraty@gmail.com>
2018-08-17 11:01:24 +0200

    Taszkok első példa

---
+++ 27TaskokBevezetes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _27TaskokBevezetes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action todo = () =>
+              {
+                  for (int i = 0; i < 10; i++)
+                  {
+                      Console.WriteLine("i:{0}",i);
+                      Thread.Sleep(500);
+                  }
+              };
+            var task = new Task(todo);
+
+            Console.WriteLine("Státusz:{0}",task.Status);
+            task.Start();
+            Console.WriteLine("Státusz:{0}", task.Status);
+            Thread.Sleep(100);
+            Console.WriteLine("Státusz:{0}", task.Status);
+            task.Wait();
+            Console.WriteLine("Státusz:{0}", task.Status);
+            Console.ReadLine();
+        }
+    }
+}
