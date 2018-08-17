708353b2f020bf0e6e911119bc21109a5a8c8f2f
szajberpiraty <szajberpiraty@gmail.com>
2018-08-17 11:29:22 +0200

    Taszk hibakezelés lehetőségek

--- 27TaskokBevezetes/Program.cs
+++ 27TaskokBevezetes/Program.cs
@@ -11,24 +11,72 @@ namespace _27TaskokBevezetes
     {
         static void Main(string[] args)
         {
+            //task státuszok
+            //teszt1();
+
+            teszt2();
+
+
+            Console.ReadLine();
+        }
+
+        private static void teszt2()
+        {
             Action todo = () =>
-              {
-                  for (int i = 0; i < 10; i++)
-                  {
-                      Console.WriteLine("i:{0}",i);
-                      Thread.Sleep(500);
-                  }
-              };
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("i:{0}", i);
+                    Thread.Sleep(100);
+                 
+                }
+                
+                throw new StackOverflowException();
+               
+            };
+           
+            var task = new Task(todo);
+            task.Start();
+            try
+            {
+                task.Wait();
+            }
+            //catch (Exception ex)
+            catch (AggregateException ex) //Ha innerexceptions-t akarunk használni a foreach-ben, akkor AggregateException kell
+            {
+
+                //Console.WriteLine("Mess:{0}",ex.Message);
+                //Egyik lehetőség a hiba megállapítására
+                //Console.WriteLine(((AggregateException)ex).Flatten());
+                //((AggregateException)ex).Flatten(); //A fa struktúrájú exception láncot kiteríthetjük
+                foreach (var e in ex.InnerExceptions)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+
+        }
+
+        private static void teszt1()
+        {
+            Action todo = () =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("i:{0}", i);
+                    Thread.Sleep(500);
+                }
+            };
             var task = new Task(todo);
 
-            Console.WriteLine("Státusz:{0}",task.Status);
+            Console.WriteLine("Státusz:{0}", task.Status);
             task.Start();
             Console.WriteLine("Státusz:{0}", task.Status);
             Thread.Sleep(100);
             Console.WriteLine("Státusz:{0}", task.Status);
             task.Wait();
             Console.WriteLine("Státusz:{0}", task.Status);
-            Console.ReadLine();
         }
     }
 }
