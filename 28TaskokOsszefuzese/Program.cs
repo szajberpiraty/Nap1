ec3b9fe1ba792da8338569ec27ee8f522da54739
szajberpiraty <szajberpiraty@gmail.com>
2018-08-17 15:46:55 +0200

    Több taszk összekapcsolása, kezdés

---
+++ 28TaskokOsszefuzese/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _28TaskokOsszefuzese
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string,int,int> Szamolas = (nev,param) =>
+            {
+                Console.WriteLine("ID{0},Param:{1},Név{2}",Thread.CurrentThread.ManagedThreadId,param,nev);
+                return 10;
+            };
+            //Hogyan kell visszaadni eredményt taskból
+            //A megoldás a generikus taszk osztály
+            //Hogyan adok paramétert a task által végrehajtott feladatnak
+            //A megoldás a Func<T> lambda, amin belül már tetszőlegesen lehet paraméterezni
+
+            var tindulo = new Task<int>(() => {return Szamolas("tindulo",15); } );
+            //Folytatás 21.43-tól
+        }
+    }
+}
