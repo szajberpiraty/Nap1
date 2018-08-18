08216875c5ff8ca027e96f88754f983b282b11e3
szajberpiraty <szajberpiraty@gmail.com>
2018-08-18 15:08:51 +0200

    RestAPI aszinkron hívás

--- 29RestAPI/Program.cs
+++ 29RestAPI/Program.cs
@@ -21,13 +21,13 @@ namespace _29RestAPI
 
             var request = new RestRequest("posts", Method.GET);
             //neg lehet adni neki, hogy milyen típusba posztoljon
-            var result = client.Execute<List<Post>>(request); 
+            var result = client.Execute<List<Post>>(request);
 
             if (result.StatusCode == System.Net.HttpStatusCode.OK)
             {//feldolgozás
                 Console.WriteLine("Státusz {0},{1}", result.StatusCode, result.StatusDescription);
                 var posts = result.Data;
-                Console.WriteLine("Postok száma:{0}",posts.Count);
+                Console.WriteLine("Postok száma:{0}", posts.Count);
                 Console.WriteLine(posts[0].body);
             }
             else
@@ -37,17 +37,28 @@ namespace _29RestAPI
 
             Console.WriteLine();
             //1-es poszt lekérése
-            var post1 = client.Execute<Post>(new RestRequest("posts/1",Method.GET));
+            var post1 = client.Execute<Post>(new RestRequest("posts/1", Method.GET));
             Console.WriteLine(post1.Data.title);
 
             //2-es poszt kommentjeinek lekérdezése
-            var commentRequest = new RestRequest("comments",Method.GET);
+            var commentRequest = new RestRequest("comments", Method.GET);
             commentRequest.AddParameter("postId", 2);
-
+            var comments = client.ExecuteAsync<List<Comment>>(commentRequest,BejovoAdatok);
+            Console.WriteLine("Aszinkron kérés elindítva");
 
 
 
             Console.ReadLine();
+        }
+
+        private static void BejovoAdatok(IRestResponse<List<Comment>> response, RestRequestAsyncHandle arg2)
+        {
+            foreach (var comment in response.Data)
+            {
+                Console.WriteLine(comment.email);
+                Console.WriteLine(comment.name);
+
+            }
         }
 
         //A JSON-ból, XML-ből Special
@@ -58,6 +69,16 @@ namespace _29RestAPI
             public string title { get; set; }
             public string body { get; set; }
         }
+
+        public class Comment
+        {
+            public int postId { get; set; }
+            public int id { get; set; }
+            public string name { get; set; }
+            public string email { get; set; }
+            public string body { get; set; }
+        }
+
 
     }
 }
