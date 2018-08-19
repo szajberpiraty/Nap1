80c3515cdc09d1971ab01fa4a5e7c72552042a0c
szajberpiraty <szajberpiraty@gmail.com>
2018-08-19 11:33:24 +0200

    Entity framework adatbázis minta kezdés

---
+++ 30CodeFirstData/models/SchoolContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30CodeFirstData.models
+{
+    //A SchoolContext-et le kell származtatni a DbContext-ből, ehhez be kell tölteni a System.Data.Entity-t, amit az EntityFramework szállít
+    class SchoolContext:DbContext
+    {
+        public DbSet<Teacher> Teachers { get; set; }
+    }
+}
---
+++ 30CodeFirstData/models/Teacher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30CodeFirstData.models
+{
+    class Teacher
+    {
+
+        /// <summary>
+        /// Minden tanár egy adott osztályban egy adott tantárgyat tanít
+        /// </summary>
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ClassCode { get; set; }
+        
+
+
+    }
+}
