2b7ffb6d07f6fee1ef3f5947aba3cc95d5a8052e
szajberpiraty <szajberpiraty@gmail.com>
2018-08-24 16:03:52 +0200

    DBFirst megközelítés, működik, de valami hibája van

---
+++ 32DataFirst/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _32DataFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var db = new SchoolContext0Entities();
+
+            foreach (var t in db.Teachers.ToList())
+            {
+                Console.WriteLine("{0},{1}", t.FirstName, t.LastName);
+                Console.WriteLine("-->Subject:{0}", t.Subject);
+                foreach (var student in t.Students.ToList())
+                {
+                    Console.WriteLine("---->Student{0}", student.Name);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 32DataFirst/models/Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _32DataFirst
+{
+    public partial class Teacher
+    {
+        public string FullName
+        {
+            get
+            {
+                return string.Format("{0},{1}",FirstName,LastName);
+            }
+
+        }
+    }
+}
